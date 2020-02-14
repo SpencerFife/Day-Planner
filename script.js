ebe5a0dcddf2efa1e0a6d1ba3d01ce8a697038c8
Spencer Fife <scottyfife17@gmail.com>
2020-02-14 14:40:26 -0800

    added psuedo code

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 $("#currentDate").text(moment().format("dddd, MMMM Do YYYY"));
+
+// once user puts text in the input field and clicks the save button
+// grab the input and store it into local storage
+// from local storage grab that information and put it back into the input field
+
+// make sure the specific input button is talking to its field. (.this())
